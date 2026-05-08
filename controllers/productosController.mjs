0ed3d909ae989be17bf6074a1620d00018a9ab57
Berni <bernalpas@gmail.com>
2026-05-08 20:35:36 -0300

    Agregamos el update de Productos

--- controllers/productosController.mjs
+++ controllers/productosController.mjs
@@ -71,7 +71,49 @@ const detallesProducto = async (req, res) => {
 }
 
 // Función para actualizar un producto específico
-const actualizarProducto = async (req, res) => {}
+const actualizarProducto = async (req, res) => {
+
+  try{
+    // obtenemos el id del producto a actualizar
+    const id = req.params.id;
+
+    // capturamos los datos del producto a actualizar
+    const nuevoProducto = {
+      nombre: req.body.nombreProducto,
+      precio: req.body.precioProducto, //verificar si es número desde el front
+      descripcion: req.body.descripcionProducto,
+      imagen: req.body.imagenProducto
+    };
+
+    console.log(nuevoProducto);
+    
+    // buscamos en la database el producto a actualizar y lo actualizamos
+    const productoAcrualizado = await ProductosModel.findByIdAndUpdate(id, nuevoProducto, {
+      new: true
+    });
+
+    console.log(productoAcrualizado);
+
+    // si no existe el producto, devolvemos un error
+    if(!productoAcrualizado){
+      return res.status(404).send({
+        message: "No existe ese producto"
+      })
+    }
+
+    // si el producto se actualizad con éxito, devolvemos un mensaje de éxito
+    console.log(productoAcrualizado);
+    
+    return res.status(200).send({
+      message: "Producto actualizado con éxito"
+    })
+
+  }catch(err){
+    console.log(err);
+    return res.status(500).send("Internal server error")
+  }
+
+}
 
 // Función para eliminar un producto específico
 const eliminarProducto = async (req, res) => {
@@ -95,7 +137,7 @@ const eliminarProducto = async (req, res) => {
 
     //verificamos si se eliminó el producto
     console.log(productoEliminado);
-    
+
     if(!productoEliminado){
       return res.status(500).send({
         message: "Error al eliminar el producto"
